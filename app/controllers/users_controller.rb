b83e0a0554fae306049e1b42e12f969820f855a0
Florian Aßmann <florian.assmann@email.de>
2008-04-21 23:09:49 +0200

    Misc tweaks and fixes.
    * User will be redirected to old request_uri after forced to login
    * Fixed styles for cmUsers
    * Added styles for cmArticles
    * Added titles to links of PagesController#index views
    * Enabled syntax highlighting and unlinked logo in ContentManagement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,18 @@ class UsersController < ApplicationController
   end
   
   def auth
+    return_uri = session[:return_uri]
     reset_session
     
     @login = Login.new params[:login]
     
     if @login.exists?
       session[:user] = @login.user_id
-      redirect_to content_management_pages_path
+      redirect_to return_uri || content_management_pages_path
     else
       render :action => :login
     end
+    session[:return_uri] = return_uri
   end
   
   def logout
