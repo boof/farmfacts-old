061e0e0fdaf9eddcd67273da1149e57d30e64ab5
Florian Aßmann <florian.assmann@email.de>
2009-02-09 08:28:50 +0100

    Moved deprecated methods into a module.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -89,14 +89,27 @@ module ApplicationHelper
     classes.at index % modulo + offset % modulo
   end
 
-  JS_INCLUDES = %w[ sh/shCore
-    sh/shBrushCSharp sh/shBrushPhp sh/shBrushJScript sh/shBrushJava
-    sh/shBrushVb sh/shBrushSql sh/shBrushXml sh/shBrushDelphi sh/shBrushPython
-    sh/shBrushRuby sh/shBrushCss sh/shBrushCpp sh/shBrushScala
-    sh/shBrushGroovy sh/shBrushBash ]
-  def syntaxhighlighter
-    opts = JS_INCLUDES + [{:cache => 'syntaxhighlighter'}]
-    stylesheet_link_tag('syntaxhighlighter') + javascript_include_tag(*opts)
+  module Deprecated
+    LAST_MODIFIED = '<span class="quiet">Last modified:</span> %s'
+    def last_modified(timestamp = nil)
+      LAST_MODIFIED % relative_date(timestamp) if timestamp
+    end
+    RELATIVE_DATE = '<span class="relative_date">%s</span>'
+    def relative_date(timestamp = nil)
+      timestamp ||= @modified_at
+      RELATIVE_DATE % timestamp.to_formatted_s if timestamp
+    end
+
+    JS_INCLUDES = %w[ sh/shCore
+      sh/shBrushCSharp sh/shBrushPhp sh/shBrushJScript sh/shBrushJava
+      sh/shBrushVb sh/shBrushSql sh/shBrushXml sh/shBrushDelphi sh/shBrushPython
+      sh/shBrushRuby sh/shBrushCss sh/shBrushCpp sh/shBrushScala
+      sh/shBrushGroovy sh/shBrushBash ]
+    def syntaxhighlighter
+      opts = JS_INCLUDES + [{:cache => 'syntaxhighlighter'}]
+      stylesheet_link_tag('syntaxhighlighter') + javascript_include_tag(*opts)
+    end
   end
+  include Deprecated
 
 end
