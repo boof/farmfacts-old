c80b3cc547300f3c8d80fbda298c5fa51e099496
Florian Aßmann <florian.assmann@email.de>
2009-02-10 09:35:04 +0100

    Simplified sweepers for upcoming pagification.

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -2,28 +2,19 @@ class PageSweeper < ActionController::Caching::Sweeper
   observe Page
 
   def before_update(page)
-    @original_path  = page.path_changed?? page.path_was : page.path
-    @link_changed   = page.title_changed? || page.path_changed?
+    @original_path = page.path_changed?? page.path_was : page.path
   end
   def after_save(page)
     expire_page '/index.html' if page.homepage?
-    expire_page @original_path if @original_path
-    expire_categories_fragments page.category_ids if @link_changed
-  end
-  def when_rejected(page)
-    expire_page '/index.html' if page.homepage?
-    expire_page page.path
-    expire_categories_fragments page.category_ids
-  end
-  def after_destroy(page)
-    expire_page '/index.html' if page.homepage?
-    expire_page page.path
-    expire_categories_fragments page.category_ids
+    expire_page @original_path || page.path
   end
 
-  protected
-  def expire_categories_fragments(ids)
-    expire_fragment %r"categories/(?:#{ ids * '|' })"
+  def default_expire(page)
+    expire_page '/index.html' if page.homepage?
+    expire_page page.path
   end
+  alias_method :after_destroy, :default_expire
+  alias_method :when_accepted, :default_expire
+  alias_method :when_rejected, :default_expire
 
 end
