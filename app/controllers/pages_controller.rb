8b2e456264065b1f00e1e3559198c854538d22ba
Florian Aßmann <flazy@fork.de>
2009-02-10 19:04:58 +0100

    PagesController for unified pagification.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,20 @@ class PagesController < ApplicationController
   ASSETS = %w[ css flv gif jpeg jpg js png swf exe cgi pl ]
 
   def show
-    @page = Page.open request.path
-    title_page @page.title
-    render :file => 'shared/page'
+    @page = Page.open request_path
+    @page.render
+  rescue ActiveRecord::RecordNotFound
+    request_path != '/404' ? redirect_to('/404') : render_404
   end
   caches_page :show
 
   protected
+  def request_path
+    request.path != '/' ? request.path : '/home'
+  end
+
   def no_assets
-    ASSETS.include? File.extname(request.path)[ 1.. -1 ] and
+    ASSETS.include? File.extname(request_path)[ 1.. -1 ] and
     render :nothing => true, :status => '404 Not Found'
   end
   before_filter :no_assets
