c3dae9dce89ed4fbba44f987e894231c2fcb1069
Florian Aßmann <flazy@fork.de>
2009-02-12 15:20:57 +0100

    Implemented YAML based preferences.

---
+++ lib/preferences.rb
@@ -0,0 +1,33 @@
+class Preferences
+  include Singleton
+
+  def self.[](key)
+    const_get(key).try :instance
+  end
+
+  def self.path
+    raise NotImplementedError
+  end
+
+  def valid?
+    raise NotImplementedError
+  end
+
+  def initialize
+    @data = YAML.load_file self.class.path
+  end
+
+  def attributes
+    @data
+  end
+  def attributes=(attributes)
+    return unless attributes.is_a? Hash
+    attributes.each { |attr, value| self.send :"#{ attr }=", value }
+  end
+
+  def save
+    yaml = YAML.dump @data
+    File.open(PATH, 'w') { |file| file << yaml } rescue false
+  end
+
+end
---
+++ lib/preferences/farm_facts.rb
@@ -0,0 +1,39 @@
+class Preferences::FarmFacts < Preferences
+
+  @path = Rails.root.join 'config', 'farmfacts.yml'
+  def self.path; @path end
+
+
+  def valid?
+    !name.blank?
+  end
+
+  def name
+    @data.fetch 'name', ''
+  end
+  def name=(name)
+    @data['name'].replace name
+  end
+
+  def server_recipients
+    @data.fetch 'server_recipients', ['admin@localhost']
+  end
+  def server_recipients=(*recipients)
+    @data['server_recipients'].replace recipients
+  end
+
+  def server_sender
+    @data.fetch 'server_sender', 'server@localhost'
+  end
+  def server_sender=(sender)
+    @data['server_sender'].replace sender
+  end
+
+  def frontpage_path
+    @data.fetch 'frontpage', '/'
+  end
+  def frontpage_path=(path)
+    @data['frontpage'].replace path
+  end
+
+end
