42f6c40dad8948ad6f8652ec166dddfdf25271df
Florian Aßmann <flazy@fork.de>
2009-02-12 15:24:05 +0100

    Added PreferencesController and Admin Dashboard to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,24 +7,35 @@ ActionController::Routing::Routes.draw do |map|
     users.logout '/logout', :action => 'logout'
   end
 
+  map.admin_dashboard '/admin', :controller => 'admin/dashboard'
   map.namespace :admin do |admin|
+
     admin.resources :pages, :except => [:destroy],
         :collection => { :bulk => :post } do |pages|
       pages.resources :attachments, :only => :create,
           :collection => { :bulk => :post }
     end
 
-    admin.resources :users, :except => [:destroy, :show],
-      :collection => { :bulk => :post }
-
-    admin.resources :categories, :except => [:destroy],
-        :collection => { :bulk => :post }
-
     admin.resources :navigations do |navigations|
       navigations.resources :nodes, :except => [:index, :show, :destroy],
           :collection => { :bulk => :post },
           :member => { :move_up => :put, :move_down => :put }
     end
+
+    admin.resources :categories, :except => [:destroy],
+        :collection => { :bulk => :post }
+
+    admin.resources :users, :except => [:destroy, :show],
+      :collection => { :bulk => :post }
+
+    admin.configure '/configure/:module_name',
+      :controller => 'preferences', :action => 'edit',
+      :module_name => 'FarmFacts',
+      :conditions => { :method => :get }
+    admin.connect '/configure/:module_name',
+      :controller => 'preferences', :action => 'update',
+      :module_name => 'FarmFacts',
+      :conditions => { :method => :put }
 
     admin.setup '/setup',
       :controller => 'setup', :action => 'new',
