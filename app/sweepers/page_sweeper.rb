526a8c9f9451b1e3d8cbae1f2db35579f2654e0e
Florian Aßmann <florian.assmann@email.de>
2009-02-15 23:17:30 +0100

    Use correct methods.

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -2,16 +2,17 @@ class PageSweeper < ActionController::Caching::Sweeper
   observe Page
 
   def before_update(page)
-    @original_path = page.path_changed?? page.path_was : page.path
+    @original_path = page.compiled_path_changed??
+      page.compiled_path_was : page.compiled_path
   end
   def after_save(page)
-    expire_page '/index.html' if page.homepage?
-    expire_page @original_path || page.path
+    expire_page '/index.html' if page.index?
+    expire_page @original_path || page.compiled_path
   end
 
   def default_expire(page)
-    expire_page '/index.html' if page.homepage?
-    expire_page page.path
+    expire_page '/index.html' if page.index?
+    expire_page page.compiled_path
   end
   alias_method :after_destroy, :default_expire
   alias_method :when_accepted, :default_expire
