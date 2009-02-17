c4f1100735e8fa41c9c889cc27af505b2fab07d8
dfue <dfue@fork.de>
2009-02-17 19:03:14 +0100

    Make ..::Stylesheet::IE::Extension include ..::Stylesheet::Extension.

--- app/models/attachment/stylesheet/ie.rb
+++ app/models/attachment/stylesheet/ie.rb
@@ -1,6 +1,7 @@
 class Attachment::Stylesheet::IE < Attachment::Stylesheet
-  
+
   module Extension
+    include ::Attachment::Stylesheet::Extension
     def to_s
       %Q'<!--[if IE]>#{ super }<![endif]-->'
     end
