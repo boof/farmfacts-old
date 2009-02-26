a0efd90cf50a2257c76326c62dba611bf4b57e79
dfue <dfue@fork.de>
2009-02-26 12:18:57 +0100

    Attachment::Image.to_s now accepts options for image_tag.

--- app/models/attachment/image.rb
+++ app/models/attachment/image.rb
@@ -3,8 +3,8 @@ class Attachment::Image < Attachment
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::AssetTagHelper
 
-  def to_s
-    image_tag attachable.to_s, :alt => disposition.humanize
+  def to_s(options = {})
+    image_tag attachable.to_s, {:alt => disposition.humanize}.update(options)
   end
 
 end
