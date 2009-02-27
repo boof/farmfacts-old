f7b237e2d9f4abc20a2b3c4b9aed6164bd2026bb
dfue <dfue@fork.de>
2009-02-27 14:58:54 +0100

    Added feature to move Elements into its controller.

--- app/controllers/admin/elements_controller.rb
+++ app/controllers/admin/elements_controller.rb
@@ -13,11 +13,22 @@ class Admin::ElementsController < Admin::Base
   def update
     save_element_and_return_to_page
   end
+
+  def move_down
+    @element.move_lower
+    redirect_to admin_themed_page_path(@themed_page)
+  end
+  def move_up
+    @element.move_higher
+    redirect_to admin_themed_page_path(@themed_page)
+  end
+
   def destroy
     @element.destroy
     
     redirect_to admin_themed_page_path(@themed_page)
   end
+
   protected
   def save_element_and_return_to_page
     @element.data = params[:element].to_hash
@@ -36,7 +47,7 @@ class Admin::ElementsController < Admin::Base
   def assign_element_by_id
     @element = @themed_page.elements.find params[:id]
   end
-  before_filter :assign_element_by_id, :only => [:edit, :update, :destroy]
+  before_filter :assign_element_by_id, :only => [:edit, :update, :move_up, :move_down, :destroy]
   def assign_element_by_name
     @element = @themed_page.elements.load params[:element_name]
   end
