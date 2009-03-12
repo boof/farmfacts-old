a6f7122edfc7461108a41280e936b6d2188f82e9
Florian Aßmann <flazy@fork.de>
2009-03-12 11:53:05 +0100

    Expire all l10n versions of stored page.

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -2,27 +2,36 @@ class PageSweeper < ActionController::Caching::Sweeper
   observe Page
 
   def before_update(page)
-    @page_path = page.path_changed?? page.path_was : page.path
+    @page_name = page.name_changed?? page.name_was : page.name
   end
-  def after_update(page)
-    expire_index if page.index?
-    expire_page "#{ @page_path }.html"
-  end
-
   def after_create(page)
     expire_index if page.index?
   end
 
   def default_expire(page)
     expire_index if page.index?
-    expire_page "#{ page.path }.html"
+
+    expire_page "/#{ name_of page }.html"
+    each_l10n(name_of(page)).each { |pp| expire_page pp }
   end
+  alias_method :after_update, :default_expire
   alias_method :after_destroy, :default_expire
   alias_method :when_accepted, :default_expire
   alias_method :when_rejected, :default_expire
 
+  protected
   def expire_index
     expire_page '/index.html'
+    each_l10n('index').each { |pp| expire_page pp }
+  end
+
+  def name_of(page)
+    @page_name || page.name
+  end
+
+  def each_l10n(name)
+    Pathname.glob("#{ File.join Rails.public_path, name }.*.html").
+    each { |pathname| yield pathname.relative_path_from(Rails.public_path) }
   end
 
 end
