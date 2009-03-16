01d7c1340bd63f69fc2d3265a6071c3e9e6aa623
Florian Aßmann <flazy@fork.de>
2009-03-16 12:02:22 +0100

    Fixed Sweeper...

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -1,6 +1,9 @@
 class PageSweeper < ActionController::Caching::Sweeper
   observe Page
 
+  # thanks Rails for its consistency...
+  PUBLIC_PATH = Pathname.new Rails.public_path
+
   def before_update(page)
     @page_name = page.name_changed?? page.name_was : page.name
   end
@@ -12,7 +15,7 @@ class PageSweeper < ActionController::Caching::Sweeper
     expire_index if page.index?
 
     expire_page "/#{ name_of page }.html"
-    each_l10n(name_of(page)).each { |pp| expire_page pp }
+    each_l10n(name_of(page)) { |pp| expire_page pp }
   end
   alias_method :after_update, :default_expire
   alias_method :after_destroy, :default_expire
@@ -22,7 +25,7 @@ class PageSweeper < ActionController::Caching::Sweeper
   protected
   def expire_index
     expire_page '/index.html'
-    each_l10n('index').each { |pp| expire_page pp }
+    each_l10n('index') { |pp| expire_page pp }
   end
 
   def name_of(page)
@@ -31,7 +34,7 @@ class PageSweeper < ActionController::Caching::Sweeper
 
   def each_l10n(name)
     Pathname.glob("#{ File.join Rails.public_path, name }.*.html").
-    each { |pathname| yield pathname.relative_path_from(Rails.public_path) }
+    each { |pathname| yield "/#{ pathname.relative_path_from PUBLIC_PATH }" }
   end
 
 end
