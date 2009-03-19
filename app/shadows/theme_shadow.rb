afb566d671de50877a99807017cdd66526e7ce72
Florian Aßmann <florian.assmann@email.de>
2009-03-19 08:54:58 +0100

    Implemented navigation stack.

--- app/shadows/theme_shadow.rb
+++ app/shadows/theme_shadow.rb
@@ -5,39 +5,8 @@ class ThemeShadow < Shadows::Base
     render_shape "#{ @origin.name }/head", :locals => locals
   end
 
-  def themed_navigation(page, tmpl = :root, level = 0)
-    buffer, index = '', 0
-
-    if root = Navigation.roots.find_by_locale(page.locale)
-      route = root.route_to_path page.path
-
-      stack = [ [ tmpl, root, route, level ] ]
-
-      while index < stack.length
-        navigation stack, index, buffer
-        index += 1
-      end
-    end
-
-    buffer
-  end
-  def navigation(stack, index, buffer)
-    buffer << case stack[index]
-    when Array
-      tmpl, root, route, level = stack[index]
-
-      locals = {
-        :root         => root,
-        :active_route => route,
-        :level        => level,
-        :stack        => stack,
-        :stack_index  => index
-      }
-
-      render_shape("#{ @origin.name }/navigations/#{ tmpl }", :locals => locals)
-    when String
-      stack[index]
-    end
+  def themed_navigation(page, template = :root)
+    NavigationStack.render @origin, page, template
   end
 
   def body(locals)
@@ -51,6 +20,56 @@ class ThemeShadow < Shadows::Base
   end
   def _load_paths
     super + [ themes_path.to_s ]
+  end
+
+  class NavigationStack < Array
+
+    def self.render(*args)
+      new(*args).render
+    end
+
+    attr_reader :route, :position
+
+    def initialize(theme, page, template = :root)
+      @theme, @position = theme, 0
+
+      if root = Navigation.roots.find_by_locale(page.locale)
+        @route = root.route_to_path page.path
+        self << [ template, root, 0 ]
+      end
+    end
+
+    def render(out = '')
+      while position < length
+        out << case item = current
+        when Array
+          template, root, level = item
+          render_navigation template, :root => root, :level => level
+        when String
+          item
+        end
+
+        @position += 1
+      end
+
+      out
+    end
+
+    def next=(item)
+      self.insert @position + 1, item
+    end
+
+    def render_navigation(template, locals)
+      locals = locals.merge :stack => self
+
+      @theme.to_s :render_shape,
+        "#{ @theme.name }/navigations/#{ template }", :locals => locals
+    end
+    protected
+    def current
+      at @position
+    end
+
   end
 
 end
