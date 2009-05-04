7f01f601bbe6929f44d24e23217978882b3b27c7
Florian Aßmann <flazy@fork.de>
2009-05-04 15:54:42 +0200

    Don't try to save anything just yet...

--- db/migrate/20090504125955_navigation_appendix.rb
+++ db/migrate/20090504125955_navigation_appendix.rb
@@ -1,16 +1,6 @@
 class NavigationAppendix < ActiveRecord::Migration
   def self.up
     change_table(:navigations) { |t| t.text :appendix }
-
-    Navigation.reset_column_information
-    Navigation.find(:all).each do |n|
-      n.appendix = {
-        :class  => n.blank?? 'hidden' : '',
-        :href   => n.path,
-        :title  => n.label
-      }
-      n.instance_eval { update_without_callbacks }
-    end
   end
 
   def self.down
