070236266c91c1d65da392093924e6518bfb4c4e
Florian Aßmann <flazy@fork.de>
2009-05-13 14:33:53 +0200

    Completed Navigation tests.

--- test/unit/navigation_test.rb
+++ test/unit/navigation_test.rb
@@ -14,6 +14,18 @@ class NavigationTest < ActiveSupport::TestCase
     end
   end
 
+  def test_that_calculated_path_returns_path_of_first_child_when_nil
+    with_route do |route|
+      route.first.path = nil
+      assert_equal route.second.path, route.first.calculated_path
+    end
+  end
+  def test_that_calculated_path_returns_path_when_set
+    with_route do |route|
+      assert_equal route.first.path, route.first.calculated_path
+    end
+  end
+
   # before save callbacks
   def test_that_path_is_completed_from_registered_path_on_save
     build_navigation_with_registered_path :except => [:path] do |navigation|
