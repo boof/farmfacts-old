ffacec83eb2117e0297ab3166f54869b0497fa7d
Florian Aßmann <florian.assmann@email.de>
2009-05-27 18:55:16 +0200

    Touch already backported to Rails 2.3.2.

--- app/models/page/component.rb
+++ app/models/page/component.rb
@@ -3,16 +3,10 @@ class Page::Component < ActiveRecord::Base
 
   belongs_to :template, :class_name => 'Theme::Template'
   validates_presence_of :template_id
-  belongs_to :composition, :class_name => 'Page::Composition'
+  belongs_to :composition, :class_name => 'Page::Composition', :touch => true,
   validates_presence_of :composition_id
 
   acts_as_list :scope => :composition
   default_scope :order => 'position'
-
-  protected
-  def touch_composition
-    composition.touch
-  end
-  after_save :touch_composition
 
 end
--- app/models/page/composition.rb
+++ app/models/page/composition.rb
@@ -8,10 +8,6 @@ class Page::Composition < ActiveRecord::Base
 
   has_many :components, :class_name => 'Page::Component', :dependent => :delete_all
 
-  def touch
-    update_attribute :updated_at => Time.now
-  end
-
   protected
   def mark_page
     page.expire
